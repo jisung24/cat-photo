18d6edd1e012c504a92453bff79b6798c28815fe
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-15 18:39:42 +0900

    [fixed✨]fetch => axios변경

--- src/api/catApi.js
+++ src/api/catApi.js
@@ -1,13 +1,10 @@
+import axios from 'axios'
+
 export const API_END_POINT = 'https://kdt-frontend.cat-api.programmers.co.kr/'
 export const request = async (id) => {
   try {
-    const res = await fetch(`${API_END_POINT}${id}`)
-    if (res.ok) {
-      const data = await res.json()
-      return data
-    } else {
-      throw new Error('Could not response from API')
-    }
+    const res = await axios.get(`${API_END_POINT}${id}`)
+    return res.data
   } catch (e) {
     throw new Error('Error fetching')
   }
