1ebc47196733a7c55839a5a1fbc9b46a397745fd
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-17 23:38:37 +0900

    [✨Refactor]Image component css추가(중간 정렬, 이미지 크기 조절)

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -11,6 +11,15 @@ export default function Image({ filePath, onClick }) {
           onClick()
         }
       })}
+      style={{
+        width: '500px',
+        height: '500px',
+        border: '5px solid black',
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
     >
       <span onClick={onClick} className="selectedImageBox__closeBtn">
         ❌
@@ -19,6 +28,7 @@ export default function Image({ filePath, onClick }) {
         src={selectedImageUrl}
         className="selectedImageBox__image"
         alt="선택된 고양이 사진"
+        style={{ width: '100%', height: '100%' }}
       />
     </div>
   )
